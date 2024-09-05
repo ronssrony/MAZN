8ba1cff6c1cea12942e5f5dbadf59a0344f49c2e
MD Abdullah AL Mamun <47145788+ronssrony@users.noreply.github.com>
2024-09-05 21:22:55 +0600

    Update product.js

--- public/javascripts/product.js
+++ public/javascripts/product.js
@@ -2,10 +2,25 @@
         const minus = document.querySelector('.volume .minus')
         const plus = document.querySelector('.volume .plus') 
         const value = document.querySelector('.volume .value')
+        let price = document.querySelector('.ttlprice').innerHTML; 
+        let discount = document.querySelector('.ttldiscount').innerHTML; 
+        let shippingfee = document.querySelector('.ttlshipping').innerHTML ; 
+        let platformfee = document.querySelector('.ttlplatform').innerHTML ;
+        let pricefield = document.querySelector('.ttlprice'); 
+        let discountfield = document.querySelector('.ttldiscount'); 
+        let shippingfeefield = document.querySelector('.ttlshipping') ; 
+        let platformfeefield = document.querySelector('.ttlplatform') ;
         plus.addEventListener('click',() => {
               var current = Number(value.innerHTML); 
               current = current + 1 ;
-              value.innerHTML = current.toString().padStart(2,"0")  ;
+          
+              pricefield.innerHTML = Number(price*current) ;
+              discountfield.innerHTML = Math.ceil(Number(discount*current)); 
+              shippingfeefield.innerHTML =Math.ceil( Number(shippingfee*current)) ;
+              platformfeefield.innerHTML = Math.ceil(Number(platformfee*current)); 
+              document.querySelector('.ttlfee').innerHTML = Number(pricefield.innerHTML) - Number(discountfield.innerHTML)+   Number(shippingfeefield.innerHTML) + Number(platformfeefield.innerHTML)
+              value.innerHTML = current.toString().padStart(2,"0");
+
         } )
         minus.addEventListener('click',() => {
             var current = Number(value.innerHTML); 
@@ -13,9 +28,18 @@
                 return
             }
             current = current - 1 ;
+            
+            pricefield.innerHTML = Number(price*current) ;
+            discountfield.innerHTML = Math.ceil(Number(discount*current)); 
+            shippingfeefield.innerHTML =Math.ceil( Number(shippingfee*current)) ;
+            platformfeefield.innerHTML = Math.ceil(Number(platformfee*current)); 
+            document.querySelector('.ttlfee').innerHTML = Number(pricefield.innerHTML) - Number(discountfield.innerHTML)+   Number(shippingfeefield.innerHTML) + Number(platformfeefield.innerHTML)
+            value.innerHTML = current.toString().padStart(2,"0");
             value.innerHTML = current.toString().padStart(2,"0") ;
            
      } )
 
+     
+      
      
       
