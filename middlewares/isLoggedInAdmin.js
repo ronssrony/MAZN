d49e2c6b511ba3cf57329933a020299f71ef9c27
MD Abdullah AL Mamun <47145788+ronssrony@users.noreply.github.com>
2024-09-05 14:35:55 +0600

    Update isLoggedInAdmin.js

--- middlewares/isLoggedInAdmin.js
+++ middlewares/isLoggedInAdmin.js
@@ -1,6 +1,7 @@
 const ownerModel = require('../models/owner-model'); 
 const jwt = require('jsonwebtoken') ; 
-const config = require('config')
+const dotenv = require('dotenv')
+dotenv.config() ;
 
 
 module.exports.isAuthorizedOwner = async function(req, res ,next){
@@ -9,7 +10,7 @@ module.exports.isAuthorizedOwner = async function(req, res ,next){
     if(token ==="") return res.status(502).send('You are Not Authorized user please Sign up or Log In') ; 
     
     try{
-       var tokenuser = jwt.verify(token, config.get('JWT_KEY')); 
+       var tokenuser = jwt.verify(token, process.env.JWT_KEY); 
     }
     catch{
       return   res.status(502).send('You are Not Authorized user please Sign up or Log In') 
