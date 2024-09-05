43a61961729f80817da4f3fafa65462813af034c
MD Abdullah AL Mamun <47145788+ronssrony@users.noreply.github.com>
2024-09-05 21:55:50 +0600

    Update isLoggedInUser.js

--- middlewares/isLoggedInUser.js
+++ middlewares/isLoggedInUser.js
@@ -7,20 +7,29 @@ dotenv.config() ;
 module.exports.isAuthorizedUser = async function(req, res ,next){
 
     const token = req.cookies.ronss ; 
-    if(token ==="") return res.status(502).send('You are Not Authorized user please Sign up or Log In') ; 
-    
-    try{
-       var tokenuser = jwt.verify(token,process.env.JWT_KEY); 
-    }
-    catch{
-      return   res.status(502).send('You are Not Authorized user please Sign up or Log In') 
-    }
+ 
 
-    const user = await userModel.findOne({ email: tokenuser.email});
-    if(!user) res.status(502).send('You are Not Authorized user please Sign up or Log In') ; 
-   else {
-       req.user = tokenuser ;
-   }
+    if(token ==="" || token===undefined) 
+    {
+      
+      req.flash('error_msg','You have to Log in First');
+     return res.status(502).redirect('/user/registration') ; 
+    } 
+    
+    else {
+      try{
+        var tokenuser = jwt.verify(token,process.env.JWT_KEY); 
+     }
+     catch{
+       return   res.status(502).redirect('/user/registration')
+     }
+ 
+     const user = await userModel.findOne({ email: tokenuser.email});
+     if(!user) res.status(502).redirect('/user/registration') ; 
+    else {
+        req.user = tokenuser ;
+    }
+    }
    next(); 
 
 } 
