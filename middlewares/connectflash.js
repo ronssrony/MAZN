be2a215a3218803993c08e48739c2d369aa0975e
MD Abdullah AL Mamun <47145788+ronssrony@users.noreply.github.com>
2024-09-05 20:41:27 +0600

    Update connectflash.js

--- middlewares/connectflash.js
+++ middlewares/connectflash.js
@@ -1,4 +1,5 @@
 module.exports.connectflash = function(req ,res , next){
        res.locals.success_msg = req.flash('success_msg'); 
        res.locals.error_msg = req.flash('error_msg') ;
+       next()
 }
